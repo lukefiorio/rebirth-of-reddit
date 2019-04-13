438e7a80a3ea483d6f393b5a0dd6a2b35ddc36b1
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-12 20:03:53 -1000

    grabbing prelim API data

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function redditListener() {
-  console.log(JSON.parse(this.responseText));
+  let responseDataArr = JSON.parse(this.responseText).data.children;
+  console.log(responseDataArr[0].data.title);
+  console.log(responseDataArr[0].data.selftext);
 }
 
 const redditReq = new XMLHttpRequest();
 redditReq.addEventListener('load', redditListener);
-redditReq.open('GET', 'https://www.reddit.com/r/dogs.json');
+redditReq.open('GET', 'https://www.reddit.com/r/pics.json');
 redditReq.send();
